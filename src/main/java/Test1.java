8e7c67aa8520f142c9d95e5d1060716d71debcbb
zuosheng <3268657155@qq.com>
2017-05-27 16:36:37 +0800

    我叫左晟测试s

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -5,6 +5,6 @@ public class Test1 {
     public static void main(String[] args) {
 
         System.out.println("hello world");
-        System.out.println("我叫左晟!大象，大象，大象~~~~~~~");
+        System.out.println("我叫左晟!大象，大象，大象~~~~~~~~");
     }
 }
