96d09ebbdaff6851701d589e22aeca025efebbe8
mabaosheng <1031465102@qq.com>
2017-05-27 16:36:36 +0800

    测试

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -3,6 +3,11 @@
  */
 public class Test1 {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+
+    }
+    public void mabaosheng(){
+        System.out.println("马保生");
     }
 }
